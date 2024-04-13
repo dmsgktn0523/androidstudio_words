a978038c6ebb1df8e123e9c689d6f61e4d73175e
dmsgktn0523 <dmsgktn0523@naver.com>
2024-04-13 21:17:35 +0900

    first version day4

---
+++ app/src/main/java/com/hello/words2/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.hello.words2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.hello.words2.databinding.ActivityAddWordsBinding
+import com.hello.words2.databinding.ContentMainBinding
+import android.content.Intent
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ContentMainBinding
+    private lateinit var adapter: ArrayAdapter<String>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ContentMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1)
+        binding.tvList.adapter = adapter
+
+        binding.btnGetText.setOnClickListener {
+            val resultText = binding.tvText.text.toString()
+            binding.tvResult.text = resultText
+            adapter.add(resultText)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.btnAdd.setOnClickListener {
+            val intent = Intent(this, AddWordsActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
